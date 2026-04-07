c54036f80bfebf34a501218520721b359aeef729
RePod <RePod@users.noreply.github.com>
2026-04-06 21:16:19 -0400

    Update APClient.h

--- APClient.h
+++ APClient.h
@@ -8,7 +8,7 @@ namespace APClient
     extern int clearGrade; // -> main
     extern std::vector<int> CheckedLocations; // -> IDHandler
     extern std::vector<int> seedIDs; // -> IDHandler
-    extern std::vector<int> recvIDs; // -> IDHandler
+    extern std::vector<int> recvIDs; // Song IDs received as items (not item IDs)
     extern std::vector<int> missingIDs; // -> IDHandler
     extern int victoryID; // -> IDHandler
     extern std::unordered_map<uint32_t, std::string> item_ap_id_to_name; // -> IDHandler
