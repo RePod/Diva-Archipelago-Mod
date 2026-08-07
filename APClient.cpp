0e53da3d46c59c150fe90745c6a5075936dcce4c
RePod <RePod@users.noreply.github.com>
2026-08-07 12:30:27 -0400

    Update APClient.cpp

--- APClient.cpp
+++ APClient.cpp
@@ -239,7 +239,7 @@ namespace APClient
 
     void PushRecvID(int64_t songID)
     {
-        if (std::find(recvIDs.begin(), recvIDs.end(), songID) != recvIDs.end() ||
+        if (std::find(recvIDs.begin(), recvIDs.end(), songID) != recvIDs.end() &&
             std::find(seedIDs.begin(), seedIDs.end(), songID) != seedIDs.end())
             return;
 
